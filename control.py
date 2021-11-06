0f91e72a006c0375d248ffa48c1fde59577cb04f
qiyuqianxai <2358341499@qq.com>
2021-11-06 17:31:23 +0800

    1106

---
+++ control.py
@@ -0,0 +1,23 @@
+import os
+from time import sleep
+import json
+from multiprocessing import Process
+states_json_path = "/workspace/go_proj/src/Ai_WebServer/algorithm_utils/model_states.json"
+model_name = "wav2lip"
+
+def run_model_server():
+    os.system("python run.py")
+
+if __name__ == '__main__':
+    running = False
+    while True:
+        with open(states_json_path,"r",encoding="utf-8")as f:
+            states = json.load(f)
+            if states[model_name] and not running:
+                os.system("ps -ef|grep python|grep -v control|cut -c 9-16|xargs kill -9")
+                p = Process(target=run_model_server,args=())
+                p.start()
+                running = True
+            elif not states[model_name]:
+                running = False
+        sleep(1)
